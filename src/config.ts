7ff1bf5bedf41f2115a17bac6f5fcdf69097a14a
Eureka <1285523226@qq.com>
2026-03-13 23:51:47 +0800

    vault backup: 2026-03-13 23:51:47

--- src/config.ts
+++ src/config.ts
@@ -30,11 +30,11 @@ export const siteConfig: SiteConfig = {
     depth: 2                // 文章目录默认显示到2级，Maximum heading depth to show in the table, from 1 to 3
   },
   favicon: [    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+    {
+      src: '/favicon/touxiang.svg',    // Path of the favicon, relative to the /public directory
+      //theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      //sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    }
   ]
 }
 
